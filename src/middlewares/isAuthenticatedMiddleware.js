c01aabc186085cb94118ade8eb1adf84fbd68dad
Briofita09 <felipe.osorio.santos@hotmail.com>
2022-07-28 22:46:50 -0300

    fix some bugs and removed console.logs

--- src/middlewares/isAuthenticatedMiddleware.js
+++ src/middlewares/isAuthenticatedMiddleware.js
@@ -3,19 +3,23 @@ import jwt from "jsonwebtoken";
 dotenv.config();
 
 async function isAuthenticatedMiddleware(req, res, next) {
-  const token = req.header("Authorization");
-  const secret = process.env.SECRET_TOKEN;
-  if (!token)
-    return res
-      .status(401)
-      .json({ message: "Você deveria estar logado para continuar" });
+  try {
+    const token = req.header("Authorization");
+    const secret = process.env.SECRET_TOKEN;
+    if (!token)
+      return res
+        .status(401)
+        .json({ message: "Você deveria estar logado para continuar" });
 
-  const validToken = token.split(" ")[1];
+    const validToken = token.split(" ")[1];
 
-  const data = jwt.verify(validToken, secret);
-  res.locals = data.user;
+    const data = jwt.verify(validToken, secret);
+    res.locals = data.user;
 
-  next();
+    next();
+  } catch (err) {
+    return res.status(500).json({ message: "Erro interno do servidor" });
+  }
 }
 
 export default isAuthenticatedMiddleware;
